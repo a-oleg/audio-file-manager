4ef4a4f583f20eac02e8b927a21a952193a8b2c9
Oleg <oleg.arhipovski@gmail.com>
2024-10-26 11:34:22 +0300

    First commit

---
+++ src/test/java/com/github/aoleg/audio_file_manager/AudioFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aoleg.audio_file_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
